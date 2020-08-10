597fb0d6eba2186608628d27530570d86a7fc6f7
Randomshot <sksghgus7175@google.com>
2020-08-10 23:40:03 +0900

    updated by Randomshot

--- app.js
+++ app.js
@@ -13,81 +13,86 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, '/public')));
 mysql.connect();
-function removeTag(content){
-    
-    
-    content = content.replace(/(<([^>]+)>)/ig,"");
-    content = content.replace(/&nbsp;/gi,"");
-    content = content.replace(/&gt;/gi,"");
-    content = content.replace(/○/gi,"");
-    content = content.replace(/\n/gi,"");
-    content = content.replace(/\./gi,"");
-    
+
+function removeTag(content) {
+
+
+    content = content.replace(/(<([^>]+)>)/ig, "");
+    content = content.replace(/&nbsp;/gi, "");
+    content = content.replace(/&gt;/gi, "");
+    content = content.replace(/○/gi, "");
+    content = content.replace(/\n/gi, "");
+    content = content.replace(/\./gi, "");
+
     return content;
 }
-function getData(){
+
+function getData() {
     const xmlurl = 'http://openapi.seoul.go.kr:8088/7a51596966736b733330767a6e5975/xml/GwanakNewsList/1/100/'
-    request.get(xmlurl,function(err, res, body){
-        if(err) console.log(err);
-        else{
-            if(res.statusCode == 200){
+    request.get(xmlurl, function(err, res, body) {
+        if (err) console.log(err);
+        else {
+            if (res.statusCode == 200) {
                 var result = body;
-                var xmlToJson = convert.xml2json(result,{
-                    compact : true,
-                    spaces : 4
+                var xmlToJson = convert.xml2json(result, {
+                    compact: true,
+                    spaces: 4
                 })
 
                 var stationCheck = -1;
                 var parseJson = JSON.parse(xmlToJson);
                 var value = parseJson.GwanakNewsList;
-                
+
                 var title;
                 var date;
                 var content;
                 var number;
-                
-                mysql.query('SELECT number FROM news ORDER BY number DESC LIMIT 1'
-                ,function(err,result){
-                    if(err) console.log(err);
-                    else{
-                        
-                        for(var i=0; i<100; i++){
-                   
-                        title = value.row[i].TITLE._text;
-                        date = value.row[i].WRITEDAY._text;
-                        content = value.row[i].CONTENT._text;
-                        number = value.row[i].SEQ._text;
-                        content = removeTag(content);
-                        date = date.substring(0,8);
-                    
-                        if(result[0].number == number){
-                           console.log("not updated");
-                            break;
-                           
+
+                mysql.query('SELECT number FROM news ORDER BY number DESC LIMIT 1', function(err, result) {
+                    if (err) console.log(err);
+                    else {
+
+                        for (var i = 0; i < 100; i++) {
+
+                            title = value.row[i].TITLE._text;
+                            date = value.row[i].WRITEDAY._text;
+                            content = value.row[i].CONTENT._text;
+                            number = value.row[i].SEQ._text;
+                            content = removeTag(content);
+                            date = date.substring(0, 8);
+
+                            if (result[0].number == number) {
+                                console.log("not updated");
+                                break;
+
+                            } else {
+                                mysql.query('INSERT INTO news (number,title,content,date) VALUES (?,?,?,?)', [number, title, content, date], function(err, result) {
+                                    if (err) console.log(err);
+                                    else {}
+                                });
+                            }
                         }
-                        else{
-                            mysql.query('INSERT INTO news (number,title,content,date) VALUES (?,?,?,?)'
-                            ,[number,title,content,date]
-                            ,function(err,result){
-                                if(err) console.log(err);
-                                else{
-                                }
-                            });
-                        }
-                      }
                     }
                 });
-                
+
             }
         }
     });
 }
 
-app.get('/getData',function(req,res){
+app.get('/getData', function(req, res) {
     getData();
 });
 
-
+app.get('/sendData',function(req,res){
+    mysql.query('SELECT * FROM news ORDER BY number DESC LIMIT 100',function(err,result){
+        if(err) console.log(err);
+        else{
+           console.log("send Data!");
+           res.send(result);
+        }
+    });
+});
 //open server
 app.get('/', function(req, res) {
     res.writeHead(200, {
@@ -96,6 +101,5 @@ app.get('/', function(req, res) {
     res.end(data);
 });
 app.listen(80, function() {
-    console.log('HackerTon server on');
+    console.log('ICT server on');
 });
-
